cf9c4a8767d83bb0b8d1302ea1147ad481617fec
Amune Israel <amuneisrael224@gmail.com>
2026-03-31 09:41:38 +0100

    update: moved files

--- scripts/day-1/linear-search.ts
+++
@@ -1,9 +0,0 @@
-export function linearSearch(haystack: number[], needle: number): boolean {
-
-    for(let i=0; i<haystack.length; i++) {
-        if(haystack[i] === needle) { 
-            return true;
-        }
-    }
-    return false;
-}
